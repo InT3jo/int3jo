89c9fa1cda73c040850791735cb9b0e39113e6bc
woonyBing <honeybye@naver.com>
2022-12-21 15:26:52 +0900

    mybatis-mapper 폴더 생성, UsersMapper.xml 생성

--- src/main/java/project3/yakdo/repository/mybatis/UsersMapper.java
+++ src/main/java/project3/yakdo/repository/mybatis/UsersMapper.java
@@ -1,8 +1,22 @@
 package project3.yakdo.repository.mybatis;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
+
+import project3.yakdo.domain.users.Users;
 
 @Mapper
 public interface UsersMapper {
+	
+	public Users insert(Users users);
+	
+	public Users selecById(int user_no);
+	
+	public Users selectByLoginEmail(String users_email);
+	
+	public List<Users> selectAll();
+	
+	public void deleteAll();
 
 }
