5e2b7ac124637dc0f947e12a3c75c438805c47ca
woonyBing <honeybye@naver.com>
2022-12-22 16:57:22 +0900

    Login 관련 Filter 작업 중

---
+++ src/main/java/project3/yakdo/filter/LoginFilter.java
@@ -0,0 +1,58 @@
+package project3.yakdo.filter;
+
+import java.io.IOException;
+
+import org.springframework.util.PatternMatchUtils;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.FilterConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import lombok.extern.slf4j.Slf4j;
+import project3.yakdo.session.SessionVar;
+
+
+@Slf4j
+public class LoginFilter implements Filter{
+
+	//로그인 하지 않아도 접속 가능한 페이지 리스트
+	private static final String[] whiteList = {"/", "/login", "/logout", "/join"};
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		log.info("LoginFilter Start");
+		
+		HttpServletRequest req = (HttpServletRequest) request;
+
+		String uri = req.getRequestURI();
+		
+		HttpServletResponse resp = (HttpServletResponse) response;
+		
+		
+		/**
+		 * 로그인 안 된 경우 => 로그인 페이지로
+		 * 
+		 */
+		if(!PatternMatchUtils.simpleMatch(whiteList, uri)) {
+			HttpSession session = req.getSession(false);
+
+			if(session == null || session.getAttribute(SessionVar.LOGIN_MEMBER) == null) {
+				log.info("로그인 없이 접근 시도 {}", uri);
+				resp.sendRedirect("/login");
+				
+				return;
+			}
+		}
+		
+		chain.doFilter(request, response);
+		
+	}
+	
+
+}
